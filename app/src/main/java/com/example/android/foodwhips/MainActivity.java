adb69e77e00206321db61850aa782f4284dcd4e0
Vincent Luu <vinvin814@gmail.com>
2017-06-28 09:34:34 -0700

    Insert app key & id for working project

--- app/src/main/java/com/example/android/foodwhips/MainActivity.java
+++ app/src/main/java/com/example/android/foodwhips/MainActivity.java
@@ -2,12 +2,81 @@ package com.example.android.foodwhips;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.AsyncTask;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.example.android.foodwhips.utilities.NetworkUtils;
+
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mFoodTextView;
+    private URL foodsUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFoodTextView = (TextView) findViewById(R.id.food_data);
+        foodsUrl = NetworkUtils.buildUrl();
+
+        loadFoodData();
     }
+
+    private void loadFoodData() {
+        new FetchFoodTask().execute();
+    }
+
+    private class FetchFoodTask extends AsyncTask<URL, Void, String> {
+
+        @Override
+        protected String doInBackground(URL... params) {
+
+            String result = null;
+
+            try {
+                result = NetworkUtils.getResponseFromHttpUrl(foodsUrl);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String foodData) {
+            if (foodData != null) {
+                mFoodTextView.setText(foodData);
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        /* Use AppCompatActivity's method getMenuInflater to get a handle on the menu inflater */
+        MenuInflater inflater = getMenuInflater();
+        /* Use the inflater's inflate method to inflate our menu layout to this menu */
+        inflater.inflate(R.menu.menu, menu);
+        /* Return true so that the menu is displayed in the Toolbar */
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.refresh) {
+            mFoodTextView.setText("");
+            loadFoodData();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
---
+++ app/src/main/java/com/example/android/foodwhips/utilities/NetworkUtils.java
@@ -0,0 +1,75 @@
+package com.example.android.foodwhips.utilities;
+
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+/**
+ * Created by Vincent on 6/27/2017.
+ */
+
+public final class NetworkUtils {
+    private static final String TAG = NetworkUtils.class.getSimpleName();
+
+    private static final String NEWS_BASE_URL = "http://api.yummly.com/v1/api/recipes?";
+
+    /* Query parameters here */
+    private final static String QUERY_PARAM_APP_ID = "_app_id";
+    private final static String appid = " insert app id here ";
+
+    private final static String QUERY_PARAM_APPKEY = "_app_key";
+    private final static String appkey = " insert app key here ";
+
+    /* Need to add something to accept third / multiple parameters */
+    private final static String QUERY_PARAM_NAME = "q";
+    private final static String param_query = "onion+soup";
+
+    public static URL buildUrl() {
+        Uri builtUri = Uri.parse(NEWS_BASE_URL).buildUpon().
+                appendQueryParameter(QUERY_PARAM_APP_ID, appid).
+                appendQueryParameter(QUERY_PARAM_APPKEY, appkey).
+                appendQueryParameter(QUERY_PARAM_NAME, param_query).
+                build();
+
+        URL url = null;
+
+        try {
+            url = new URL(builtUri.toString());
+        }
+        catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        Log.v(TAG, "Built URI: " + url);
+
+        return url;
+    }
+
+    /* This method goes to the API and grabs the JSON string */
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+
+
+}
